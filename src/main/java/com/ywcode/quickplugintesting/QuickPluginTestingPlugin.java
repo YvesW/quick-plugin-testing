8a527bfaa780a1bf6f1d67d23f478c1297ab5587
YvesW <yves_w@ymail.com>
2023-01-11 23:40:30 +0100

    Add tabs

--- src/main/java/com/ywcode/quickplugintesting/QuickPluginTestingPlugin.java
+++ src/main/java/com/ywcode/quickplugintesting/QuickPluginTestingPlugin.java
@@ -3,7 +3,7 @@ package com.ywcode.quickplugintesting;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.Client;
+import net.runelite.api.*;
 import net.runelite.api.events.*;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -303,35 +303,35 @@ public class QuickPluginTestingPlugin extends Plugin {
 
 	@Subscribe
 	public void onVarClientStrChanged(VarClientStrChanged varClientStrChanged) {
-}
+	}
 
 	@Subscribe
 	public void onVolumeChanged(VolumeChanged volumeChanged) {
-}
+	}
 
 	@Subscribe
 	public void onWallObjectDespawned(WallObjectDespawned wallObjectDespawned) {
-}
+	}
 
 	@Subscribe
 	public void onWallObjectSpawned(WallObjectSpawned wallObjectSpawned) {
-}
+	}
 
 	@Subscribe
 	public void onWidgetClosed(WidgetClosed widgetClosed) {
-}
+	}
 
 	@Subscribe
 	public void onWidgetLoaded(WidgetLoaded widgetLoaded) {
-}
+	}
 
 	@Subscribe
 	public void onWorldChanged(WorldChanged worldChanged) {
-}
+	}
 
 	@Subscribe
 	public void onWorldListLoad(WorldListLoad worldListLoad) {
-}
+	}
 
 	@Provides
 	QuickPluginTestingConfig provideConfig(ConfigManager configManager)
