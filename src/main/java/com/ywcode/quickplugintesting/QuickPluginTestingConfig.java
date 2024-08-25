e710c2fb4a5d9b975086ede675474a2cc6cdf6ea
YvesW <7929021+YvesW@users.noreply.github.com>
2024-08-26 00:09:44 +0200

    imports: reduce the amount of wildcard imports

--- src/main/java/com/ywcode/quickplugintesting/QuickPluginTestingConfig.java
+++ src/main/java/com/ywcode/quickplugintesting/QuickPluginTestingConfig.java
@@ -1,6 +1,9 @@
 package com.ywcode.quickplugintesting;
 
-import net.runelite.client.config.*;
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Keybind;
 
 @ConfigGroup("qptesting")
 public interface QuickPluginTestingConfig extends Config
